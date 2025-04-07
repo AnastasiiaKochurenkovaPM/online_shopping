498bf66afadc8d05803d5192dba9ed4a155bdb96
dr10n9 <dr10n9@gmail.com>
2025-04-07 16:58:59 +0300

    WC

--- frontend/App.js
+++ frontend/App.js
@@ -6,37 +6,13 @@ import config from './config.js'
 import { Platform } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
-import * as Device from 'expo-device';
-import { firebase } from '@react-native-firebase/messaging';
-// import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions'
 export default function App() {
   useEffect(() => {
     async function init() {
       const os = Platform.OS;
 
       if (os === 'web') {
-        const permission = await Notification.requestPermission();
-        if (permission !== "granted") {
-          console.log("Notification permissions not granted");
-          return;
-        }
-        console.log('permissions granted')
-        if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-          try {
-            const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { type: 'module' });
-            console.log('SW registered');
-            const app = initializeApp(config.firebase);
-            const messaging = getMessaging(app);
-            const token = await getToken(messaging, {
-              vapidKey: config.firebase.vapidKey,
-              serviceWorkerRegistration: registration
-            })
-            console.log({ token })
-
-          } catch (error) {
-            console.error('failed to register SW or retrieve token', error);
-          }
-        }
+        registerForWeb();
       } else if (os === 'android') {
         registerForPushNotificationsAsync();
       } else {
@@ -87,4 +63,30 @@ async function registerForPushNotificationsAsync() {
     console.error(error)
   }
 
+}
+
+
+async function registerForWeb() {
+  const permission = await Notification.requestPermission();
+  if (permission !== "granted") {
+    console.log("Notification permissions not granted");
+    return;
+  }
+  console.log('permissions granted')
+  if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { type: 'module' });
+      console.log('SW registered');
+      const app = initializeApp(config.firebase);
+      const messaging = getMessaging(app);
+      const token = await getToken(messaging, {
+        vapidKey: config.firebase.vapidKey,
+        serviceWorkerRegistration: registration
+      })
+      console.log({ token })
+
+    } catch (error) {
+      console.error('failed to register SW or retrieve token', error);
+    }
+  }
 }
